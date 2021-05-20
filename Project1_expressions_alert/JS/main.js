00590ceca5624821219944bcd5c7f2d898ae6abf
Jason <AarsZach@yahoo.com>
2021-05-20 13:06:46 -0600

    Update main.js
    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,3 +1,4 @@
+/*
 // // Basic alert function
 // alert("Hello, World!");
 
@@ -37,3 +38,28 @@
 
 // // Arithmatic
 // document.write(3 + 7 + 8 + 2 + 9 + 1 + 4 + 6);
+*/
+
+/* ==============================================================
+    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
+============================================================== */
+// [X] Utilize the window.alert() and document.write() methods.
+// [X] Assign string values to two variables and concatenate the variables together.
+// [X] Assign a color to at least one variable using the fontcolor() method.
+// [X] Write an expression.
+// [ ] Add comments throughout your JavaScript explaining your code
+
+// Initialize two varaiables
+var str_1 = "Set A",
+    str_2 = "Set B";
+
+// set the font color of the str_2 to red
+var str_2 = str_2.fontcolor("red");
+
+// A variable expression
+var expression = 37 + 73;
+
+window.alert("37 + 73 = " + expression);
+window.alert("Set string str_2 to:\n" + str_2);
+
+document.write(str_1 + "\n" + str_2);
