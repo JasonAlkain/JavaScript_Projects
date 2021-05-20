82f9a6e5a145caad788fb35cf8290877abeae9df
Jason <AarsZach@yahoo.com>
2021-05-20 12:10:20 -0600

    Challenges and more

---
+++ Challenges/JS/main.js
@@ -0,0 +1,3 @@
+// Concatenating strings together then adding it to the web page
+var concat_Str = "Concatenated String: " + " \"A\" " + " \"B\" " + " \"C\" ";
+document.write(concat_Str);
--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,7 +1,23 @@
-alert("Hello, World!");
-window.alert("Hello, World! (window.alert())");
-document.write('Hello, World!');
+// // Basic alert function
+// alert("Hello, World!");
 
-var A = "This is a string!";
+// // Simple window.alert() function
+// window.alert("Hello, World! (window.alert())");
 
-document.write(A);
+// // Writes 'Hello, World!' to the webpage.
+// document.write('Hello, World!');
+
+// // Creates a variable named 'A' and then passes it to the webpage
+// var A = "This is a string!";
+// document.write(A);
+
+// // Creates a variable named 'str_Display' and then passes it to window.alert
+// var str_Display = "This is a triumph! [^v^]";
+// window.alert(str_Display);
+
+// // Creates a variable named 'str_Display_Con' with an escape character and then passes it to window.alert
+// var str_Display_Con = "This is a triumph! [^v^]\nWe do what we must because we can!";
+// window.alert(str_Display_Con);
+
+// // Concatenating strings together then adding it to the web page
+// document.write(" \"A\" " + " \"B\" " + " \"C\" ");
