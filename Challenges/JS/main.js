49bfdd61d934208e08e8fac9cbdf95e535c85bdd
Jason <AarsZach@yahoo.com>
2021-05-20 13:47:17 -0600

    EVENT CHALLENGE

--- Challenges/JS/main.js
+++ Challenges/JS/main.js
@@ -1,3 +1,36 @@
 // Concatenating strings together then adding it to the web page
 var concat_Str = "Concatenated String: " + " \"A\" " + " \"B\" " + " \"C\" ";
 document.write(concat_Str);
+
+/* ==============================================================
+    EVENT CHALLENGE
+============================================================== */
+
+function BTN_OnClick() {
+    // clear the element text
+    document.getElementById('date').innerHTML = "";
+    // split the date at every space
+    var dateSplit = Date().split("");
+    // Start the new string with
+    var strNew = "";
+    // Iterate through the
+    dateSplit.forEach(ds => {
+        if (ds == " ") {
+            strNew += "<br>" + ds;
+        } else if (ds == "(" || ds == ")") {
+            strNew += "";
+        } else {
+            strNew += ds;
+        }
+    });
+
+    strNew += "";
+
+    document.getElementById('date').innerHTML = strNew;
+    // document.getElementById('date').innerHTML = Date();
+}
+
+
+/* ==============================================================
+    END EVENT CHALLENGE
+============================================================== */
