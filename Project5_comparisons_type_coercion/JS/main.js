fc05b3c077e36b3b32ba39e89d39533f11f4e376
Jason <AarsZach@yahoo.com>
2021-05-21 17:05:57 -0600

    BOOLEAN ASSIGNMENT

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -5,6 +5,7 @@
 function OnLoad() {
     BTN_TypeCoercion();
     BTN_Infinite();
+    BTN_Boolean();
 }
 
 function BTN_TypeCoercion() {
@@ -18,4 +19,12 @@ function BTN_TypeCoercion() {
 function BTN_Infinite() {
     document.getElementById("Infinity").innerHTML = "2E310 = " + (2E310);
     document.getElementById("NegInfinity").innerHTML = "-3E310 = " + (-3E310);
+}
+
+function BTN_Boolean() {
+    var _A = 15;
+    var _B = 5;
+    document.getElementById("Boolean").innerHTML = "";
+    document.getElementById("Boolean").innerHTML += "Boolean result: ";
+    document.getElementById("Boolean").innerHTML += (_A < _B);
 }
