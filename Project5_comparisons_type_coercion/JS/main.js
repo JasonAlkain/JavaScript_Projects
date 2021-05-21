6edd329b47d599dbdad5eda5e7efeec7af86d42e
Jason <AarsZach@yahoo.com>
2021-05-21 17:41:15 -0600

    DOUBLE & TRIPLE EQUAL SIGNS ASSIGNMENT

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -3,13 +3,15 @@
 =================================== */
 
 function OnLoad() {
-    BTN_TypeCoercion();
-    BTN_Infinite();
-    BTN_Boolean();
+    TypeCoercion();
+    Infinite();
+    _Boolean();
     ConsoleCMD();
+    DoubleEqual();
+    TripleEqual();
 }
 
-function BTN_TypeCoercion() {
+function TypeCoercion() {
     var _A = "5";
     var _B = 5;
     document.getElementById("TypeCoercion").innerHTML = "";
@@ -17,12 +19,12 @@ function BTN_TypeCoercion() {
     document.getElementById("TypeCoercion").innerHTML += (typeof(_A) == typeof(_B));
 }
 
-function BTN_Infinite() {
+function Infinite() {
     document.getElementById("Infinity").innerHTML = "2E310 = " + (2E310);
     document.getElementById("NegInfinity").innerHTML = "-3E310 = " + (-3E310);
 }
 
-function BTN_Boolean() {
+function _Boolean() {
     var _A = 15;
     var _B = 5;
     document.getElementById("Boolean").innerHTML = "";
@@ -32,4 +34,30 @@ function BTN_Boolean() {
 
 function ConsoleCMD() {
     console.log(783 + 327);
+}
+
+function DoubleEqual() {
+    var _A = "5";
+    var _B = 5;
+    var _C = 5;
+    document.getElementById("DoubleEqual_A").innerHTML = "";
+    document.getElementById("DoubleEqual_A").innerHTML += "Double Equal A:<br> 5 === 5 <br>";
+    document.getElementById("DoubleEqual_A").innerHTML += (typeof(_B) == typeof(_C));
+
+    document.getElementById("DoubleEqual_B").innerHTML = "";
+    document.getElementById("DoubleEqual_B").innerHTML += "Double Equal B:<br> \"5\" === 5 <br>";
+    document.getElementById("DoubleEqual_B").innerHTML += (typeof(_A) == typeof(_B));
+}
+
+function TripleEqual() {
+    var _A = "5";
+    var _B = 5;
+    var _C = 5;
+    document.getElementById("TripleEqual_A").innerHTML = "";
+    document.getElementById("TripleEqual_A").innerHTML += "Triple Equal A:<br> 5 === 5 <br>";
+    document.getElementById("TripleEqual_A").innerHTML += (typeof(_B) === typeof(_C));
+
+    document.getElementById("TripleEqual_B").innerHTML = "";
+    document.getElementById("TripleEqual_B").innerHTML += "Triple Equal B:<br> \"5\" === 5 <br>";
+    document.getElementById("TripleEqual_B").innerHTML += (typeof(_A) === typeof(_B));
 }
