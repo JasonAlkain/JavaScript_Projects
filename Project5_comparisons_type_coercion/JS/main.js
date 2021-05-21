d7791421b7e6abc7ba92b4bd3a2d82a6bf10769f
Jason <AarsZach@yahoo.com>
2021-05-21 17:02:32 -0600

    Small fixes and INFINITY ASSIGNMENT

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -2,8 +2,20 @@
     Project5: TYPE COERCION
 =================================== */
 
+function OnLoad() {
+    BTN_TypeCoercion();
+    BTN_Infinite();
+}
+
 function BTN_TypeCoercion() {
     var _A = "5";
     var _B = 5;
-    document.getElementById("TypeCoercion").innerHTML = (typeof(_A) == typeof(_B));
+    document.getElementById("TypeCoercion").innerHTML = "";
+    document.getElementById("TypeCoercion").innerHTML += "Type Coercion: ";
+    document.getElementById("TypeCoercion").innerHTML += (typeof(_A) == typeof(_B));
+}
+
+function BTN_Infinite() {
+    document.getElementById("Infinity").innerHTML = "2E310 = " + (2E310);
+    document.getElementById("NegInfinity").innerHTML = "-3E310 = " + (-3E310);
 }
