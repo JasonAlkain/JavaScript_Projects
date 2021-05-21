5101404df5114d4ef770833834fb3c5b17601e46
Jason <AarsZach@yahoo.com>
2021-05-20 19:18:59 -0600

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,9 +1,45 @@
-/*==============================
- * FUNCTION ASSIGNMENT
- ==============================*/
+/* ==============================================================
+    FUNCTION ASSIGNMENT
+============================================================== */
 var counter = 0;
 
 function BTN_OnClick() {
     counter++;
     document.getElementById('point').innerHTML = "Click Count: " + counter;
 }
+/* ==============================================================
+    END FUNCTION ASSIGNMENT
+============================================================== */
+
+/* ==============================================================
+    EVENT CHALLENGE
+============================================================== */
+
+function BTN_Operator() {
+    // clear the element text
+    document.getElementById('date').innerHTML = "";
+    // split the date at every space
+    var dateSplit = Date().split("");
+    // Start the new string with
+    var strNew = "";
+    // Iterate through the
+    dateSplit.forEach(ds => {
+        if (ds == " ") {
+            strNew += "<br>" + ds;
+        } else if (ds == "(" || ds == ")") {
+            strNew += "";
+        } else {
+            strNew += ds;
+        }
+    });
+
+    strNew += "";
+
+    document.getElementById('date').innerHTML = strNew;
+    // document.getElementById('date').innerHTML = Date();
+}
+
+
+/* ==============================================================
+    END EVENT CHALLENGE
+============================================================== */
