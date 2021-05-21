862064c8256684bd0d4f2c9c1ec6753dcfb83d4d
Jason <AarsZach@yahoo.com>
2021-05-21 16:29:36 -0600

    TYPE COERCION ASSIGNMENT

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,9 @@
+/* ===================================
+    Project5: TYPE COERCION
+=================================== */
+
+function BTN_TypeCoercion() {
+    var _A = "5";
+    var _B = 5;
+    document.getElementById("TypeCoercion").innerHTML = (typeof(_A) == typeof(_B));
+}
