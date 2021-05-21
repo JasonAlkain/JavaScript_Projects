71172d4de005777e50441f83fe6ca2640737fa53
Jason <AarsZach@yahoo.com>
2021-05-21 11:06:11 -0600

    ASSIGNMENTS
    MULTIPLICATION AND DIVISION & MULTIPLE OPERATORS ASSIGNMENT

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -23,8 +23,41 @@ function BTN_MathSubtraction() {
     var _MathID = document.getElementById("Math_Sub");
     _MathID.innerHTML = "";
     // Variables
-    counter++;
     mass -= 1;
     // Print to page 
     _MathID.innerHTML = "Subtracts 1 with each Click.<br>Mass: " + mass + "<br>";
+}
+
+function BTN_MathMultiplication() {
+    var _MathID = document.getElementById("Math_Mul");
+    _MathID.innerHTML = "";
+    // Variables
+    var rnd_A = Math.floor(Math.floor(Math.random() * 9) + 1);
+    var rnd_B = Math.floor(Math.floor(Math.random() * 9) + 1);
+    var math_Mul = rnd_A * rnd_B;
+    // Print to page 
+    _MathID.innerHTML = "math_Mul: " + rnd_A + " * " + rnd_B + " = " + math_Mul;
+}
+
+function BTN_MathDivision() {
+    var _MathID = document.getElementById("Math_Div");
+    _MathID.innerHTML = "";
+    // Variables
+    var pi_A = 22;
+    var pi_B = 7;
+    var math_Div = pi_A / pi_B;
+    // Print to page 
+    _MathID.innerHTML = "math_Div: " + pi_A + " / " + pi_B + " = " + math_Div;
+}
+
+function BTN_MultipleOperators() {
+    var _MathID = document.getElementById("Math_MO");
+    _MathID.innerHTML = "";
+    // Variables
+    var pi_A = 22;
+    var pi_B = 7;
+    var r = 7;
+    var C = 2 * (pi_A / pi_B) * r;
+    // Print to page 
+    _MathID.innerHTML = "math_MO: <br>2 * (" + pi_A + " / " + pi_B + ") * " + r + " = <br>" + C;
 }
