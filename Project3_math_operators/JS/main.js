bd1d04076ceec83afe1bfe748f686f4703fe1157
Jason <AarsZach@yahoo.com>
2021-05-21 10:42:07 -0600

    SUBTRACTION ASSIGNMENT

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -4,6 +4,7 @@
 
 var counter = 0;
 var money = 0;
+var mass = 100;
 
 function BTN_MathOperator() {
     var _MathID = document.getElementById("Math");
@@ -16,4 +17,14 @@ function BTN_MathOperator() {
     // Print to page 
     _MathID.innerHTML = "Day: " + counter + "<br>";
     _MathID.innerHTML += "Balance: " + money + "&yen;";
+}
+
+function BTN_MathSubtraction() {
+    var _MathID = document.getElementById("Math_Sub");
+    _MathID.innerHTML = "";
+    // Variables
+    counter++;
+    mass -= 1;
+    // Print to page 
+    _MathID.innerHTML = "Subtracts 1 with each Click.<br>Mass: " + mass + "<br>";
 }
