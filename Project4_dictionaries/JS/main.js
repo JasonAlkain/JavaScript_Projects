ede78d370d4c0128047095a82b4ddca8bba41065
Jason <AarsZach@yahoo.com>
2021-05-21 16:18:50 -0600

    DELETE ASSIGNMENT

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -20,3 +20,18 @@ function BTN_Dict() {
         document.getElementById("Dictionary").innerHTML += "[ " + e + ": " + v + " ]<br>";
     }
 }
+
+function BTN_Dict_RM() {
+    document.getElementById("Dict_RM").innerHTML = "Age: ";
+
+    var Person = {
+        First_Name: "Zachary",
+        Last_Name: "Aars",
+        Race: "Human",
+        Age: "27",
+        Gender: "Male"
+    }
+
+    delete Person.Age;
+    document.getElementById("Dict_RM").innerHTML += Person.Age;
+}
