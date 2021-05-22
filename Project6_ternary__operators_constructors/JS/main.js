03f797948ddd1b55a17ea6b5acda5c33bf29e807
Jason <AarsZach@yahoo.com>
2021-05-22 12:50:16 -0600

    NESTED FUNCTIONS ASSIGNMENT

--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -1,5 +1,5 @@
 /* =========================================
-    KEYWORDS
+    TERNARY CHALLENGE
 ========================================= */
 
 function ternaryChallenge() {
@@ -29,4 +29,70 @@ function myFunction() {
         Erik.Vehicle_Color + "-colored " +
         Erik.Vehicle_Model + " manufactured in " +
         Erik.Vehicle_Year;
+}
+
+/* =========================================
+    RESERVED KEYWORD CHALLEGNE
+========================================= */
+function Player(first, last, race, job, strength, wisdom) {
+    this.firstName = first;
+    this.lastName = last;
+    this.race = race;
+    this.job = job;
+    this.strength = strength;
+    this.wisdom = wisdom;
+}
+
+var player_A = new Player("Jason", "Alkain", "Human", "Fighter", 17, 14);
+var player_B = new Player("Hitomi", "Saiyori", "Elf", "Ranger", 14, 18);
+
+function reservedChallenge() {
+    /**/
+    var reserved = document.getElementById("reserved");
+    /**/
+    var divider = "<br><div style=\"margin: 0 auto; border-top: 1px solid black;\"></div><br>";
+    /**/
+    reserved.innerHTML = divider;
+    /**/
+    reserved.innerHTML += "[ Player A ]<br>";
+    for (var e in player_A) {
+        // Return the value for the current key
+        var v = player_A[e];
+        // Display both the value and the current key
+        reserved.innerHTML += "" + e + ": " + v + "<br>";
+    }
+    reserved.innerHTML += divider;
+    /**/
+    reserved.innerHTML += "[ Player B ]<br>";
+    for (var e in player_B) {
+        // Return the value for the current key
+        var v = player_B[e];
+        // Display both the value and the current key
+        reserved.innerHTML += "" + e + ": " + v + "<br>";
+    }
+    reserved.innerHTML += divider;
+    /**/
+}
+/* =========================================
+    NESTED FUNCTIONS
+========================================= */
+
+var nestedCounter = 0;
+
+function nestedClick() {
+    /**/
+    var Nested = document.getElementById("Nested_Function");
+    Nested.innerHTML = nested();
+    /**/
+    function nested() {
+        /**/
+        var divider = "<br><div style=\"margin: 0 auto; border-top: 1px solid black;\"></div><br>";
+        var clicked = "I have been clicked: ";
+
+        function incrementCounter() { nestedCounter++; }
+        incrementCounter();
+        return divider + clicked + "<br>" + nestedCounter + divider;
+
+    }
+    /**/
 }
