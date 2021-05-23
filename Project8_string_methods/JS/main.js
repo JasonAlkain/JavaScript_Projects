60193f2748948e6dc0d7d4a29c94dfbd314eac8b
Jason <AarsZach@yahoo.com>
2021-05-22 19:16:23 -0600

    MORE METHODS CHALLENGE

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -17,11 +17,10 @@ function BTN_CONCAT() {
     /* */
     concatStr.innerHTML = wholeStr;
 }
+
 /* =========================================
     CONCAT() METHOD
 ========================================= */
-
-var globalVar = "Global";
 
 function BTN_SLICER() {
     /* */
@@ -34,3 +33,69 @@ function BTN_SLICER() {
     /* */
     concatStr.innerHTML = CenterStr.slice(24, CenterStr.length - 1);
 }
+
+/* =========================================
+    MORE METHODS CHALLENGE
+========================================= */
+
+/* =========================================
+    UPPER AND LOWER METHOD
+========================================= */
+
+function BTN_UPPER() {
+    /* */
+    var strElement = document.getElementById("upperLower");
+    /* */
+    var strMod = strElement.innerText.toUpperCase();
+    /* */
+    /* */
+    strElement.innerHTML = strMod;
+}
+
+function BTN_LOWER() {
+    /* */
+    var strElement = document.getElementById("upperLower");
+    /* */
+    var strMod = strElement.innerText.toLowerCase();
+    /* */
+    /* */
+    strElement.innerHTML = strMod;
+}
+
+/* =========================================
+    SEARCH() METHOD
+========================================= */
+
+function BTN_SEARCH() {
+    /* */
+    var Sercher = document.getElementById("searched");
+    var Result = document.getElementById("result");
+    var SearchTerm = document.getElementById("searchTerm").value;
+    /* */
+    console.log(Sercher.innerHTML);
+    console.log(SearchTerm);
+    /* */
+    function myFunction(strSearch, term) {
+        var n;
+        if (term != "") {
+            n = strSearch.search(term);
+            if (n == -1) {
+                n = "Unable to find a match.<br>Try again?";
+            } else {
+                n = "Found a match!<br>" + term;
+            }
+
+        } else {
+            n = "Search bar is empty. Please enter something.".toUpperCase();
+        }
+        return n;
+    }
+
+    var result = myFunction(Sercher.innerHTML, SearchTerm);
+    /* */
+    Result.innerHTML = result;
+}
+
+/* =========================================
+    END MORE METHODS CHALLENGE
+========================================= */
