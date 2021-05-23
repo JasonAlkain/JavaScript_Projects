210ac6041aa57d7466fd99519b9983b3c44f7256
Jason <AarsZach@yahoo.com>
2021-05-22 18:32:16 -0600

    SLICE() METHOD ASSIGNMENT

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -17,3 +17,20 @@ function BTN_CONCAT() {
     /* */
     concatStr.innerHTML = wholeStr;
 }
+/* =========================================
+    CONCAT() METHOD
+========================================= */
+
+var globalVar = "Global";
+
+function BTN_SLICER() {
+    /* */
+    var concatStr = document.getElementById("slicer");
+    /* */
+    var divOpener = "<div style=\"margin: 0 auto; border-top: 1px solid black; border-bottom: 1px solid black; border-radius: 8px;\">";
+    var divCloser = "</div>";
+    var CenterStr = "String is at the center of the concatenating method.";
+    /* */
+    /* */
+    concatStr.innerHTML = CenterStr.slice(24, CenterStr.length - 1);
+}
