4544662e6b6bfa67b7f83c1311540ed1fdeb1e28
Jason <AarsZach@yahoo.com>
2021-06-02 13:52:44 -0600

    Update tictactoe.js
    bug fixing

--- TicTacToe_Game/JS/tictactoe.js
+++ TicTacToe_Game/JS/tictactoe.js
@@ -14,10 +14,10 @@ function PlaceX_O(squareNumber) {
         //
         if (activePlayer === 'X') {
             //
-            select.style.backgroundImage = 'url("Images/x.png)';
+            select.style.backgroundImage = 'url("./Images/x.png")';
         } else {
             //
-            select.style.backgroundImage = 'url("Images/o.png)';
+            select.style.backgroundImage = 'url("./Images/o.png")';
         }
         //
         selectedSquares.push(squareNumber + activePlayer);
@@ -26,18 +26,18 @@ function PlaceX_O(squareNumber) {
         //
         if (activePlayer === 'X') {
             //
-            activePlayer === 'O';
+            activePlayer = 'O';
         } else {
             //
-            activePlayer === 'X';
+            activePlayer = 'X';
         }
 
         // Now we do audio!!
-        PlayAudio('./Sound/place.mp3');
+        //PlayAudio('./Sound/place.mp3');
         // 
         if (activePlayer === 'O') {
             //
-            DisableClick();
+            //DisableClick();
             //
             setTimeout(() => { ComputersTurn(); }, 1000);
         }
@@ -69,44 +69,57 @@ function PlaceX_O(squareNumber) {
 
 function CheckWinConditions() {
     //
-    if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
+    if (ArrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
     //
-    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
+    else if (ArrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
     //
-    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
+    else if (ArrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
     //
-    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
+    else if (ArrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
     //
-    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
+    else if (ArrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
     //
-    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
+    else if (ArrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
     //
-    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
+    else if (ArrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
     //
-    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
+    else if (ArrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
     //
     //
-    if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
+    else if (ArrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
     //
-    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
+    else if (ArrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
     //
-    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
+    else if (ArrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
     //
-    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
+    else if (ArrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
     //
-    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
+    else if (ArrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
     //
-    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558) }
+    else if (ArrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558) }
     //
-    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90) }
+    else if (ArrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90) }
     //
-    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520) }
+    else if (ArrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520) }
     //
     //
     else if (selectedSquares.length >= 9) {
         //
-        PlayAudio('./Sound/tie.mp3');
+        //PlayAudio('./Sound/tie.mp3');
         //
         setTimeout(() => { ResetGame(); }, 1000);
+    }
+
+
+    //
+    //
+    function ArrayIncludes(squareA, squareB, squareC) {
+        //
+        const A = selectedSquares.includes(squareA);
+        const B = selectedSquares.includes(squareB);
+        const C = selectedSquares.includes(squareC);
+        //
+        //
+        if (A === true && B === true && C === true) { return true; }
     }
 }
