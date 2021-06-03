e66483e547d4bf147af8935ec463ef521de0407f
Jason <AarsZach@yahoo.com>
2021-06-02 18:07:47 -0600

    Update tictactoe.js
    TIC TAC TOE PART SEVEN ASSIGNMENT

--- TicTacToe_Game/JS/tictactoe.js
+++ TicTacToe_Game/JS/tictactoe.js
@@ -69,38 +69,38 @@ function PlaceX_O(squareNumber) {
 
 function CheckWinConditions() {
     //
-    if (ArrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
+    if (ArrayIncludes('0X', '1X', '2X')) { DrawWinLine(50, 100, 558, 100) }
     //
-    else if (ArrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
+    else if (ArrayIncludes('3X', '4X', '5X')) { DrawWinLine(50, 304, 558, 304) }
     //
-    else if (ArrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
+    else if (ArrayIncludes('6X', '7X', '8X')) { DrawWinLine(50, 508, 558, 508) }
     //
-    else if (ArrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
+    else if (ArrayIncludes('0X', '3X', '6X')) { DrawWinLine(100, 50, 100, 558) }
     //
-    else if (ArrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
+    else if (ArrayIncludes('1X', '4X', '7X')) { DrawWinLine(304, 50, 304, 558) }
     //
-    else if (ArrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
+    else if (ArrayIncludes('2X', '5X', '8X')) { DrawWinLine(508, 50, 508, 558) }
     //
-    else if (ArrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
+    else if (ArrayIncludes('6X', '4X', '2X')) { DrawWinLine(100, 508, 510, 90) }
     //
-    else if (ArrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
+    else if (ArrayIncludes('0X', '4X', '8X')) { DrawWinLine(100, 100, 520, 520) }
     //
     //
-    else if (ArrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
+    else if (ArrayIncludes('0O', '1O', '2O')) { DrawWinLine(50, 100, 558, 100) }
     //
-    else if (ArrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
+    else if (ArrayIncludes('3O', '4O', '5O')) { DrawWinLine(50, 304, 558, 304) }
     //
-    else if (ArrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
+    else if (ArrayIncludes('6O', '7O', '8O')) { DrawWinLine(50, 508, 558, 508) }
     //
-    else if (ArrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
+    else if (ArrayIncludes('0O', '3O', '6O')) { DrawWinLine(100, 50, 100, 558) }
     //
-    else if (ArrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
+    else if (ArrayIncludes('1O', '4O', '7O')) { DrawWinLine(304, 50, 304, 558) }
     //
-    else if (ArrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558) }
+    else if (ArrayIncludes('2O', '5O', '8O')) { DrawWinLine(508, 50, 508, 558) }
     //
-    else if (ArrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90) }
+    else if (ArrayIncludes('6O', '4O', '2O')) { DrawWinLine(100, 508, 510, 90) }
     //
-    else if (ArrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520) }
+    else if (ArrayIncludes('0O', '4O', '8O')) { DrawWinLine(100, 100, 520, 520) }
     //
     //
     else if (selectedSquares.length >= 9) {
@@ -137,5 +137,70 @@ function DisableClick() {
     //
     setTimeout(() => {
         body.style.pointerEvents = 'auto';
+    }, 1000);
+}
+
+function DrawWinLine(coordX1, coordX2, coordY1, coordY2) {
+    //
+    const canvas = document.getElementById('win-lines');
+    //
+    const c = canvas.getContext('2d');
+    //
+    let x1 = coordX1,
+        x2 = coordX2,
+        y1 = coordY1,
+        y2 = coordY2,
+        x = x1,
+        y = y1;
+
+    function AnimateLineDrawing() {
+        //
+        const animationLoop = requestAnimationFrame(AnimateLineDrawing);
+        //
+        c.clearRect(0, 0, 608, 608);
+        //
+        c.beginPath();
+        //
+        c.moveTo(x1, y1);
+        //
+        c.moveTo(x, y);
+        //
+        c.lineWidth = 10;
+        //
+        c.strokeStyle = 'rgba(70, 255, 3, 0.8)';
+        //
+        c.stroke();
+        //
+        if (x1 <= x2 && y1 <= y2) {
+            //
+            if (x < x2) { x += 10; }
+            if (y < y2) { y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop()); }
+        }
+        //
+        if (x1 <= x2 && y1 >= y2) {
+            //
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop()); }
+        }
+    }
+
+    function Clear() {
+        //
+        const animationLoop = requestAnimationFrame(Clear);
+        //
+        c.clearRect(0, 0, 608, 608);
+    }
+    //
+    DisableClick();
+    //
+    PlayAudio('./Sound/winGame.mp3');
+    // 
+    AnimateLineDrawing();
+    //
+    setTimeout(() => {
+        Clear();
+        ResetGame();
     }, 1000);
 }
