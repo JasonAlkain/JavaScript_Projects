cb7219bab49929e9778d8ca934c416f5e46c18ba
Jason <AarsZach@yahoo.com>
2021-06-09 19:01:42 -0600

    VEGETABLE MENU
    PIZZA MENU VEGETABLE ASSIGNMENT

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,18 +1,20 @@
-function GetReceipt() {
-    //
-    //
-    var text1 = "<h3>You Ordered:</h3>";
-    var runningTotal = 0;
-    var sizeTotal = 0;
-    var sizeArray = document.getElementsByClassName("size");
+// These variables are common and can be moved outside
+var text1 = "<h3>You Ordered:</h3>";
+var runningTotal = 0;
 
+function GetSize() {
+    //
+    var sizeTotal = 0;
+    //
+    var sizeArray = document.getElementsByClassName("size");
+    //
     for (let i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 += selectedSize + "<br>";
         }
     }
-
+    //
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
@@ -26,14 +28,14 @@ function GetReceipt() {
     }
 
     runningTotal = sizeTotal;
-
-
-    GetTopping(runningTotal, text1);
 }
 
-function GetTopping(runningTotal, text1) {
+function GetTopping() {
+    //
     var toppingTotal = 0;
+    //
     var selectedTopping = [];
+    //
     var toppingArray = document.getElementsByClassName("toppings");
 
     for (let j = 0; j < toppingArray.length; j++) {
@@ -52,6 +54,40 @@ function GetTopping(runningTotal, text1) {
     }
     runningTotal += toppingTotal;
 
+}
+
+function GetVegetables() {
+    var vegetableTotal = 0;
+    var selectedVegetables = [];
+    var vegetableArray = document.getElementsByClassName("vegetables");
+
+    for (let j = 0; j < vegetableArray.length; j++) {
+        if (vegetableArray[j].checked) {
+            selectedVegetables.push(vegetableArray[j].value);
+            text1 += vegetableArray[j].value + "<br>";
+        }
+    }
+
+    var vegetableCount = selectedVegetables.length;
+    if (vegetableCount > 1) {
+        vegetableTotal = (vegetableCount - 1);
+    } else {
+        vegetableTotal = 0;
+    }
+    runningTotal += vegetableTotal;
+
+}
+
+// Update
+function UpdateDocument() {
+    text1 = "<h3>You Ordered:</h3>";
+    runningTotal = 0;
+
+    GetSize();
+    GetTopping();
+    GetVegetables();
+
     document.getElementById("showText").innerHTML = text1;
     document.getElementById("totalPrice").innerHTML = "<h3> Total: <strong>$" + runningTotal + ".00</strong></h3>";
+
 }
